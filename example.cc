8e840e1728b58aff5b17b6a993bce18e191b12ae
Zhihao Yuan <zhihao.yuan@rackspace.com>
2013-05-09 13:16:00 -0400

    A C stream can not be reoriented; reopen it.

--- example.cc
+++ example.cc
@@ -22,10 +22,6 @@ int main() {
 	printf("%i %d %u %i %d %u\n", -1, -1, -1, -1, -1, -1);
 	cout << vputf("%c %d %#.2f\n", std::make_tuple(64, 64, 0.1));
 	printf("%c %d %#.2f\n", 64, 64, 0.1);
-	wcout << putf(L"% 012d|%.f\n", 1234567, f);
-	wprintf(L"% 012d|%.f\n", 1234567, f);
-	wcout << vputf(L"%-+012d|%+014p\n", std::make_pair(1234567, &f));
-	wprintf(L"%-+012d|%+014p\n", 1234567, &f);
 	cout << putf("%12.4s|%14.2s|%.s\n", s.data(), s, s.data());
 	printf("%12.4s|%14.2s|%.s\n", s.data(), s.data(), s.data());
 	std::array<int, 7> args {{ -12, 6, 64, 12, 64, -6, 64 }};
@@ -33,6 +29,9 @@ int main() {
 	printf("%*.*d|%#.*X|% 0*d\n", -12, 6, 64, 12, 64, -6, 64);
 	cout << putf("%1$d:%2$.*3$d:%4$*4$.*3$d\n", 12, 6, 2, 6);
 	printf("%1$d:%2$.*3$d:%4$*4$.*3$d\n", 12, 6, 2, 6);
-	//cout << vputf("%#x %#x %#x %#x\n", "bye!");
-	return 0;
+	freopen(nullptr, "w", stdout);
+	wcout << putf(L"% 012d|%.f\n", 1234567, f);
+	wprintf(L"% 012d|%.f\n", 1234567, f);
+	wcout << vputf(L"%-+012d|%+014p\n", std::make_pair(1234567, &f));
+	wprintf(L"%-+012d|%+014p\n", 1234567, &f);
 }
